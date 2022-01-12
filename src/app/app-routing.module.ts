aaefc3cae6d0ed3b038e7cc07bda7d60f36afd67
antoninocalisto <antuninosantos@gmail.com>
2022-01-12 12:01:18 -0300

    [Antonino Calisto] - Tela de login - Falta deixar responsivo, saber logar e colocar o esquecer senha

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import {HomeComponent} from './home/home.component'
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [ AuthGuard ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
